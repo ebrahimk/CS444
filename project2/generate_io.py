5b7bb908b4535868500ebb865a7b84d2ee0935c1
Kamron <ebrahimk@oregonstate.edu>
2018-05-06 14:16:04 -0700

    tweaked script

--- project2/generate_io.py
+++ project2/generate_io.py
@@ -13,12 +13,12 @@ mystr = "file"
 
 #if we only generate I/O in one directory all of the requests occur in roughly the same sector
 #we thus want to skip around the directory structure
-for z in range(10):	
+for z in range(30):	
 	if not os.path.exists(directory):
    		 os.makedirs(directory)
 	os.chdir(directory)
 	
-	for x in range(100):
+	for x in range(50):
 		file_pointer = open(mystr+str(x)+".txt","w+")
 		for y in range(10):
 			newChar = random.choice(string.ascii_lowercase)
@@ -27,10 +27,10 @@ for z in range(10):
 		file_pointer.write("\n")
 		file_pointer.close()
 
-	for x in range(20):
-        	file_pointer = open(mystr+str(x)+".txt","r")
-       		file_pointer.read()  
-		file_pointer.close()	
+	#for x in range(20):
+        #	file_pointer = open(mystr+str(x)+".txt","r")
+       	#	file_pointer.read()  
+	#	file_pointer.close()	
 
 	os.chdir('..') #move to folder in which script exists 
 	shutil.rmtree(directory)
@@ -40,7 +40,7 @@ for z in range(10):
                  os.makedirs(directory)
         os.chdir(directory)
 
-        for x in range(100):
+        for x in range(50):
                 file_pointer = open(mystr+str(x)+".txt","w+")
                 for y in range(10):
                         newChar = random.choice(string.ascii_lowercase)
@@ -49,10 +49,10 @@ for z in range(10):
                 file_pointer.write("\n")
                 file_pointer.close()
 
-        for x in range(20):
-                file_pointer = open(mystr+str(x)+".txt","r")
-                file_pointer.read()
-                file_pointer.close()	
+        #for x in range(20):
+        #        file_pointer = open(mystr+str(x)+".txt","r")
+        #        file_pointer.read()
+        #        file_pointer.close()	
 
 	os.chdir('..') #move to the parent directory 
 	shutil.rmtree(directory)
@@ -67,7 +67,7 @@ for z in range(10):
                  os.makedirs(directory2)
         os.chdir(directory2)
 
-        for x in range(100):
+        for x in range(50):
                 file_pointer = open(mystr+str(x)+".txt","w+")
                 for y in range(10):
                         newChar = random.choice(string.ascii_lowercase)
@@ -76,10 +76,10 @@ for z in range(10):
                 file_pointer.write("\n")
                 file_pointer.close()
 
-        for x in range(20):
-                file_pointer = open(mystr+str(x)+".txt","r")
-                file_pointer.read()
-                file_pointer.close()
+        #for x in range(20):
+        #        file_pointer = open(mystr+str(x)+".txt","r")
+        #        file_pointer.read()
+        #        file_pointer.close()
 
 	os.chdir('..') 
 	shutil.rmtree(directory2)
