1e0a70714033e3c42466c9cb0756e83ec4b185f9
Kamron <ebrahimk@oregonstate.edu>
2018-05-05 17:38:21 -0700

    finished rough draft

--- project2/changes.sh
+++ project2/changes.sh
@@ -4,6 +4,6 @@ origin=`git config remote.origin.url`
 base=`dirname "$origin"`/`basename "$origin" .git`
 
 # Output LaTeX table in chronological order
-echo "\\begin{tabular}{l l l l}\\textbf{Link} & \\textbf{Date} & \\textbf{Author} & \\textbf{Description}\\\\\\hline"
-git log --pretty=format:"\\href{$base/commit/%H}{%h} & %ad & %an & %s\\\\\\hline" --reverse
+echo "\\begin{tabular}{l l l}\\textbf{Date} & \\textbf{Author} & \\textbf{Description}\\\\\\hline"
+git log --pretty=format:"%ad & %an & %s\\\\\\hline" --reverse
 echo "\end{tabular}"
