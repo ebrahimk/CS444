a9ff13699c4b05a396633d9c1af2687303454435
Kamron <ebrahimk@oregonstate.edu>
2018-05-06 21:17:37 -0700

    final testing script

--- project2/gen_IO.cpp
+++ project2/gen_IO.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-const unsigned long long size=1024ULL*1024ULL;
+const unsigned long long size=128ULL*128ULL;
 unsigned long long a[size];
 
 int main(){
@@ -17,25 +17,25 @@ for(int i = 0; i< 10; i++){
                 fwrite(a,1,size*sizeof(unsigned long long), pFile);
         }
         fclose(pFile);
-
-        pFile1=fopen("IOoutput1.tmp","wb");
+       
+	pFile1=fopen("IOoutput1.tmp","wb");
         for(unsigned long long j=0; j<128; ++j){
                 fwrite(a,1,size*sizeof(unsigned long long), pFile1);
         }
         fclose(pFile1);
-
+	
         pFile2=fopen("IOoutput2.tmp","wb");
         for(unsigned long long j=0; j<128; ++j){
                 fwrite(a,1,size*sizeof(unsigned long long), pFile2);
         }
-        fclose(pFile2);
+      fclose(pFile2);
 
-        pFile=fopen("IOoutput3.tmp","wb");
+        pFile3=fopen("IOoutput3.tmp","wb");
         for(unsigned long long j=0; j<128; ++j){
                 fwrite(a,1,size*sizeof(unsigned long long), pFile3);
         }
         fclose(pFile3);
-
+	
         pFile4=fopen("IOoutput.tmp","wb");
         for(unsigned long long j=0; j<128; ++j){
                 fwrite(a,1,size*sizeof(unsigned long long), pFile4);
