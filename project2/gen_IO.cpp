0863e544d3befe233e40ac0afa04309ad1754201
Kamron <ebrahimk@oregonstate.edu>
2018-05-06 19:51:48 -0700

    trying new I/O generation method

--- project2/gen_IO.cpp
+++ project2/gen_IO.cpp
@@ -10,33 +10,34 @@ int main(){
 	FILE*  pFile3;
 	FILE*  pFile4;
 
-for(int i = 0; i< 10; i++){
+
+for(int i = 0; i< 20; i++){
         pFile=fopen("IOoutput.tmp","wb");
-        for(unsigned long long j=0; j<256; ++j){
+        for(unsigned long long j=0; j<128; ++j){
                 fwrite(a,1,size*sizeof(unsigned long long), pFile);
         }
         fclose(pFile);
 
         pFile1=fopen("IOoutput1.tmp","wb");
-        for(unsigned long long j=0; j<256; ++j){
+        for(unsigned long long j=0; j<128; ++j){
                 fwrite(a,1,size*sizeof(unsigned long long), pFile1);
         }
         fclose(pFile1);
 
         pFile2=fopen("IOoutput2.tmp","wb");
-        for(unsigned long long j=0; j<256; ++j){
+        for(unsigned long long j=0; j<128; ++j){
                 fwrite(a,1,size*sizeof(unsigned long long), pFile2);
         }
         fclose(pFile2);
 
         pFile=fopen("IOoutput3.tmp","wb");
-        for(unsigned long long j=0; j<256; ++j){
+        for(unsigned long long j=0; j<128; ++j){
                 fwrite(a,1,size*sizeof(unsigned long long), pFile3);
         }
         fclose(pFile3);
 
         pFile4=fopen("IOoutput.tmp","wb");
-        for(unsigned long long j=0; j<256; ++j){
+        for(unsigned long long j=0; j<128; ++j){
                 fwrite(a,1,size*sizeof(unsigned long long), pFile4);
         }
         fclose(pFile4);
